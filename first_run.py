f7a0de58e61712cc217b589aeef07df14f47dee3
KimMY5159 <92921923+KimMY5159@users.noreply.github.com>
2022-11-03 16:51:42 +0900

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -10,12 +10,26 @@ cur = conn.cursor(pymysql.cursors.DictCursor)
 @app.route('/page=<page_num>')
 def hello(page_num):
     page_num = int(page_num.strip())
-    # MySQL 서버에 접속하기
+    res = []
+    sql = "SELECT COUNT(*) FROM movies_221102"
+    cur.execute(sql)
+    total_results = int(cur.fetchone())
+    if total_results%30 == 0:
+      total_pages = total_results/30
+    else:
+      total_pages = int(total_results/30)+1
+    
+    
     sql = f"SELECT * FROM movies_221102 ORDER BY popularity DESC LIMIT 30 OFFSET {30*page_num-30}"
-    # MySQL 명령어 실행하기
     cur.execute(sql)
     # 전체 row 가져오기
-    res = cur.fetchall()
+    data = cur.fetchall()
+    jsonify(data)
+    res = jsonify(
+       page : page_num,
+       results: data,
+       total_pages: ,total_pages
+       total_results: total_results)
     # Flask에서 제공하는 json변환 함수
     return jsonify(res)
 
